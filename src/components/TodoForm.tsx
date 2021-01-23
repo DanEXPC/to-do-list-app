22bae9c6fcf10e2fa7a18d5ef16bdab62de38cb0
Danylo <danylosmahliuk@gmail.com>
2021-01-23 10:20:23 +0200

    Rewriting input processing functionality using useRef

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,24 +1,28 @@
-import React, {useState} from 'react'
+import React, {useRef} from 'react'
 
 export const ToDoForm: React.FC = () => {
-  const [title, setTitle] = useState<string>('')
+  // const [title, setTitle] = useState<string>('')
+  const ref = useRef<HTMLInputElement>(null)
 
-  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(event.target.value)
-  }
+  // const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setTitle(event.target.value)
+  // }
 
   const keyPressHandler = (event: React.KeyboardEvent) => {
     if (event.key === 'Enter') {
-      console.log(title)
-      setTitle('')
+      console.log(ref.current!.value)
+      ref.current!.value = ''
+      // console.log(title)
+      // setTitle('')
     }
   }
 
   return (
     <div className="input-field mt2">
         <input 
-          onChange={changeHandler}
-          value={title} 
+          // onChange={changeHandler}
+          // value={title} 
+          ref={ref}
           type="text" 
           id="title" 
           placeholder="Enter task title"
