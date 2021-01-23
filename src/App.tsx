3d01dd24b994fafeba04b62431a78c4f85cc65ba
Danylo <danylosmahliuk@gmail.com>
2021-01-23 13:26:32 +0200

    Splitting the application into separate pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { Navbar } from './components/Navbar'
-import { ToDoForm } from './components/TodoForm'
-import { ToDoList } from './components/TodoList'
-import { ITodo } from './interfaces'
-
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState<ITodo[]>([])
-
-  useEffect(() => {
-    const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[]
-
-    setTodos(saved)
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-  }, [todos])
-
-  const addHandler = (title: string) => {
-    const newTodo: ITodo = {
-      title: title,
-      id: Date.now(),
-      completed: false
-    }
-    // setTodos([newTodo, ...todos])
-    setTodos(prev => [newTodo, ...prev])
-  }
-
-  const toggleHandler = (id: number) => {
-    setTodos(prev => prev.map(todo => {
-      if(todo.id === id) {
-        return { 
-          ...todo,
-          completed: !todo.completed
-        }
-      }
-      return todo
-    }))
-  }
-
-  const removeHandler = (id: number) => {
-    const shouldRemove = window.confirm('Are you sure you want to delete the task?')
-    if (shouldRemove) {
-      setTodos(prev => prev.filter(todo => todo.id !== id))
-    } 
-  }
+  
 
   return <>
     <Navbar />
     <div className="container">
-      <ToDoForm onAdd={addHandler} />
-
-      <ToDoList todos={todos} onToggle={toggleHandler} onRemove={removeHandler} />
+      
     </div>
   </>
 }
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AboutPage: React.FC = () => {
+  return(
+    <>
+      <h1>Information page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a at necessitatibus modi laboriosam eos saepe, vitae ipsam ipsum voluptatum.</p>
+      <button className="btn">Go to the list of tasks</button>
+    </>
+  )
+}
