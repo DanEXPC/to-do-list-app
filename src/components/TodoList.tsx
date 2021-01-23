eda00bc0abe5acdb9e617959568d07f34e1b5e40
Danylo <danylosmahliuk@gmail.com>
2021-01-23 11:07:15 +0200

    Adding a feature to change the status of a task and remove it

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,9 +3,11 @@ import { ITodo } from '../interfaces'
 
 type TodoListProps = {
     todos: ITodo[]
+    onToggle: (id: number) => void // 1 variant
+    onRemove(id: number): void // 2 variant
 }
 
-export const ToDoList: React.FC<TodoListProps> = ({ todos }) => {
+export const ToDoList: React.FC<TodoListProps> = ({ todos, onToggle, onRemove }) => {
   return (
     <ul>
       {todos.map(todo => {
@@ -16,9 +18,9 @@ export const ToDoList: React.FC<TodoListProps> = ({ todos }) => {
           return (
             <li className={classes.join(' ')} key={todo.id}>
               <label>
-                  <input type="checkbox" checked={todo.completed}/>
+                  <input type="checkbox" checked={todo.completed} onChange={onToggle.bind(null, todo.id)}/>
                   <span>{todo.title}</span>
-                  <i className="material-icons red-text">delete</i>
+                  <i className="material-icons red-text" onClick={() => onRemove(todo.id)}>delete</i>
               </label>
             </li>
           )
