5530672018183367cae73f8662d564dcbb27a81b
Danylo <danylosmahliuk@gmail.com>
2021-01-23 10:10:08 +0200

    Adding state to input change

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const ToDoForm: React.FC = () => {
+  const [title, setTitle] = useState<string>('')
 
-    return (
-        <div className="input-field mt2">
-            <input type="text" id="title" placeholder="Enter task title"/>
-            <label htmlFor="title" className="active">Enter task title</label>
-        </div>
-    )
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value)
+  }
+
+  return (
+    <div className="input-field mt2">
+        <input 
+          onChange={changeHandler}
+          value={title} 
+          type="text" 
+          id="title" 
+          placeholder="Enter task title"/>
+        <label htmlFor="title" className="active">Enter task title</label>
+    </div>
+  )
 }
